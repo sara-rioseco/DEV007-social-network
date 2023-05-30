1aadc651358d153ed8e18bb041815e5f5e90770c
sara-rioseco <sara.rioseco@gmail.com>
2023-05-30 10:28:53 -0400

    adds firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 // Este es el punto de entrada de tu aplicacion
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-import { myFunction } from './lib/index.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyAoPzifXuWI56JUEwPFZDnkPnbVyF7tLa4",
+  authDomain: "pets--diaries.firebaseapp.com",
+  projectId: "pets--diaries",
+  storageBucket: "pets--diaries.appspot.com",
+  messagingSenderId: "954679174408",
+  appId: "1:954679174408:web:cbf851119a71a5f81f78bd",
+  measurementId: "G-BKTBMV3M64",
+};
 
-myFunction();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/*import { myFunction } from "./lib/index.js";
+
+myFunction();*/
