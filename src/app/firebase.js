1074ef7884e0390b0305bfe3a73f8df01ef425cf
sara-rioseco <sara.rioseco@gmail.com>
2023-05-30 12:58:39 -0400

    adds files and imgs

---
+++ src/app/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoPzifXuWI56JUEwPFZDnkPnbVyF7tLa4",
+  authDomain: "pets--diaries.firebaseapp.com",
+  projectId: "pets--diaries",
+  storageBucket: "pets--diaries.appspot.com",
+  messagingSenderId: "954679174408",
+  appId: "1:954679174408:web:cbf851119a71a5f81f78bd",
+  measurementId: "G-BKTBMV3M64",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAoPzifXuWI56JUEwPFZDnkPnbVyF7tLa4",
-  authDomain: "pets--diaries.firebaseapp.com",
-  projectId: "pets--diaries",
-  storageBucket: "pets--diaries.appspot.com",
-  messagingSenderId: "954679174408",
-  appId: "1:954679174408:web:cbf851119a71a5f81f78bd",
-  measurementId: "G-BKTBMV3M64",
-};
+import { myFunction } from "./lib/index.js";
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-/*import { myFunction } from "./lib/index.js";
-
-myFunction();*/
+myFunction();
