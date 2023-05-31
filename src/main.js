9b1ff1c8767cfb180cc0aa43b7f15ec17cfa4f2b
sara-rioseco <sara.rioseco@gmail.com>
2023-05-30 23:37:44 -0400

    adds routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,67 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from "./lib/index.js";
+// import { myFunction } from "./lib/index.js";
 
-myFunction();
+// myFunction();
+
+import Home from "./components/home.js"
+import Login from "./components/login.js"
+import Register from "./components/register.js"
+import Timeline from "./components/timeline.js"
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
+
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    { path: '/', view: () => console.log('Estás en home.') },
+    { path: '/login', view: () => console.log('Estás en login.') },
+    { path: '/register', view: () => console.log('Estás en register.') },
+    { path: '/timeline', view: () => console.log('Estás en timeline.') },
+  ];
+
+  // Test each route for potential match
+  const potentialMatches = routes.map(route => {
+    return {
+      route: route,
+      isMatch: location.pathname.match(pathToRegex(route.path))
+    };
+  });
+
+  let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      result: [location.pathname]
+    };
+  }
+  const view = new match.route.view(getParams(match));
+
+  document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener("click". e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+    router();
+});
