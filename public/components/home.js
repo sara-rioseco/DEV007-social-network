2f7b58185075eebdf45efd3185de86357d973986
sara-rioseco <sara.rioseco@gmail.com>
2023-06-19 17:50:46 -0400

    adds firebase hosting

---
+++ public/components/home.js
@@ -0,0 +1,41 @@
+export const Home = (onNavigate) => {
+  const homeDiv = document.createElement('div');
+  const headerDiv = document.createElement('div');
+  const subtitle = document.createElement('h2');
+  const contentDiv = document.createElement('div');
+  const loginBttn = document.createElement('button');
+  const registerBttn = document.createElement('button');
+  const contentImgDiv = document.createElement('div');
+  const backgroundImg = document.createElement('img');
+  const heartImg = document.createElement('img');
+
+  heartImg.src = 'img/logo-title-red.png';
+  heartImg.classList.add('heart');
+  homeDiv.className = 'home-div';
+  headerDiv.className = 'header-div';
+  contentDiv.className = 'content-div';
+  contentImgDiv.className = 'content-img';
+  backgroundImg.src = 'img/background_pets.png';
+  backgroundImg.className = 'corner-image';
+
+  subtitle.textContent = '¡Bienvenid@ a la red social para los amantes de los animales!';
+  loginBttn.textContent = 'Ingresar';
+  registerBttn.textContent = 'Registrarme';
+  loginBttn.classList.add('homeBttn');
+  registerBttn.classList.add('homeBttn');
+
+  loginBttn.addEventListener('click', () => onNavigate('/login'));
+  registerBttn.addEventListener('click', () => onNavigate('/register'));
+
+  contentDiv.appendChild(headerDiv);
+  contentDiv.appendChild(loginBttn);
+  contentDiv.appendChild(registerBttn);
+  homeDiv.appendChild(heartImg);
+  contentImgDiv.appendChild(backgroundImg);
+  homeDiv.appendChild(contentImgDiv);
+  headerDiv.appendChild(subtitle);
+  homeDiv.appendChild(contentDiv);
+  homeDiv.appendChild(contentDiv);
+
+  return homeDiv;
+};
---
+++ public/components/profile.js
@@ -0,0 +1,33 @@
+export const Profile = (onNavigate) => {
+  const profileDiv = document.createElement('div');
+  const headerDiv = document.createElement('div');
+  const subtitle = document.createElement('h2');
+  const contentDiv = document.createElement('div');
+  const homeBttn = document.createElement('button');
+  const contentImgDiv = document.createElement('div');
+  const backgroundImg = document.createElement('img');
+  const heartImg = document.createElement('img');
+
+  heartImg.src = 'img/logo-title-red.png';
+  heartImg.classList.add('heart');
+  profileDiv.className = 'home-div';
+  headerDiv.className = 'header-div';
+  contentDiv.className = 'content-div';
+  contentImgDiv.className = 'content-img';
+  backgroundImg.src = 'img/background_pets.png';
+  backgroundImg.className = 'corner-image';
+  homeBttn.classList.add('loginBttn');
+
+  subtitle.textContent = 'Bienvenid@, este es tu perfil';
+  homeBttn.textContent = 'Volver al inicio';
+  homeBttn.addEventListener('click', () => onNavigate('/'));
+  contentDiv.appendChild(headerDiv);
+  profileDiv.appendChild(heartImg);
+  contentImgDiv.appendChild(backgroundImg);
+  profileDiv.appendChild(contentImgDiv);
+  headerDiv.appendChild(subtitle);
+  contentDiv.appendChild(homeBttn);
+  profileDiv.appendChild(contentDiv);
+
+  return profileDiv;
+};
